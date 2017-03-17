455ce9a0e141082563f5013ebf86c08125b95297
msuzuki <led.centipede@gmail.com>
2017-03-17 22:32:58 +0900

    load html file func add

--- BrowserApp/File.cs
+++ BrowserApp/File.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace BrowserApp
 {
     class File
     {
+        //HTMLファイルをロード
+        public static string getTextFileText(string filepath)
+        {
+            string body = "";
+            if(!filepath.Equals(""))
+            {
+                StreamReader sr = new StreamReader(
+                    filepath,
+                    System.Text.Encoding.GetEncoding("UTF-8")
+                );
+                body = sr.ReadToEnd();
+                sr.Close();
+            }
+
+            return body;
+        }
     }
 }
