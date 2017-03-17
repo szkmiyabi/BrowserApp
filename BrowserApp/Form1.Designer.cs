cb72e9da8f196b6f54404c0335d638a6a88e585a
msuzuki <led.centipede@gmail.com>
2017-03-17 23:27:13 +0900

    基本的なコントロール部品を追加

--- BrowserApp/Form1.Designer.cs
+++ BrowserApp/Form1.Designer.cs
@@ -28,12 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.statusBarText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.urlCombo = new System.Windows.Forms.ComboBox();
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.prevButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.reloadButton = new System.Windows.Forms.Button();
+            this.statusBar.SuspendLayout();
+            this.menuBar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusBar
+            // 
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBarText});
+            this.statusBar.Location = new System.Drawing.Point(0, 409);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(913, 22);
+            this.statusBar.TabIndex = 2;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // statusBarText
+            // 
+            this.statusBarText.Name = "statusBarText";
+            this.statusBarText.Size = new System.Drawing.Size(0, 17);
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(85, 22);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.開くToolStripMenuItem.Text = "開く";
+            // 
+            // menuBar
+            // 
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(913, 26);
+            this.menuBar.TabIndex = 3;
+            this.menuBar.Text = "menuStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.browser);
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(913, 383);
+            this.panel1.TabIndex = 4;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.urlCombo);
+            this.flowLayoutPanel1.Controls.Add(this.urlText);
+            this.flowLayoutPanel1.Controls.Add(this.prevButton);
+            this.flowLayoutPanel1.Controls.Add(this.nextButton);
+            this.flowLayoutPanel1.Controls.Add(this.reloadButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(913, 31);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 31);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(913, 352);
+            this.browser.TabIndex = 1;
+            // 
+            // urlCombo
+            // 
+            this.urlCombo.FormattingEnabled = true;
+            this.urlCombo.Location = new System.Drawing.Point(3, 3);
+            this.urlCombo.Name = "urlCombo";
+            this.urlCombo.Size = new System.Drawing.Size(83, 20);
+            this.urlCombo.TabIndex = 0;
+            // 
+            // urlText
+            // 
+            this.urlText.Location = new System.Drawing.Point(92, 3);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(495, 19);
+            this.urlText.TabIndex = 1;
+            // 
+            // prevButton
+            // 
+            this.prevButton.Location = new System.Drawing.Point(593, 3);
+            this.prevButton.Name = "prevButton";
+            this.prevButton.Size = new System.Drawing.Size(38, 23);
+            this.prevButton.TabIndex = 2;
+            this.prevButton.Text = "戻る";
+            this.prevButton.UseVisualStyleBackColor = true;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(637, 3);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(35, 23);
+            this.nextButton.TabIndex = 3;
+            this.nextButton.Text = "進む";
+            this.nextButton.UseVisualStyleBackColor = true;
+            // 
+            // reloadButton
+            // 
+            this.reloadButton.Location = new System.Drawing.Point(678, 3);
+            this.reloadButton.Name = "reloadButton";
+            this.reloadButton.Size = new System.Drawing.Size(38, 23);
+            this.reloadButton.TabIndex = 4;
+            this.reloadButton.Text = "更新";
+            this.reloadButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(913, 431);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.menuBar);
+            this.MainMenuStrip = this.menuBar;
+            this.Name = "Form1";
+            this.Text = "BrowserApp";
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel statusBarText;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ComboBox urlCombo;
+        private System.Windows.Forms.TextBox urlText;
+        private System.Windows.Forms.Button prevButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button reloadButton;
     }
 }
 
